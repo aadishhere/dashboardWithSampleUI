2d5acfb62dadbe8dfa5e5e208c31f70178fe79d2
Aadish Jain <aadishhere@gmail.com>
2025-05-14 11:33:45 +0530

    Initial Commit

---
+++ dashboardWithSampleUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dashboardWithSampleUI
+//
+//  Created by Aadish Jain on 14/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dashboardWithSampleUI/dashboardWithSampleUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  dashboardWithSampleUIApp.swift
+//  dashboardWithSampleUI
+//
+//  Created by Aadish Jain on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct dashboardWithSampleUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
