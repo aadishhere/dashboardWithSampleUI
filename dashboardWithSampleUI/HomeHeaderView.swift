f3a0a1dbafa321566fde539355e3dd8057e9ee43
Aadish Jain <84015581+aadishhere@users.noreply.github.com>
2025-05-26 09:36:14 +0530

    Optimised

--- dashboardWithSampleUI/HomeHeaderView.swift
+++ dashboardWithSampleUI/HomeHeaderView.swift
@@ -1,7 +1,19 @@
 //
-//  Untitled.swift
+//  HomeHeaderView.swift
 //  dashboardWithSampleUI
 //
 //  Created by Aadish Jain on 14/05/25.
 //
 
+import SwiftUI
+
+struct HomeHeaderView: View {
+    var title: String
+
+    var body: some View {
+        Text(title)
+            .font(.headline)
+            .padding(.horizontal)
+            .padding(.top, 10)
+    }
+}
