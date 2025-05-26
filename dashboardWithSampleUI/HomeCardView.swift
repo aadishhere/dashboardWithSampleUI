f80b18021ce8e7e91b7c541a64acf7809dde89b4
Aadish Jain <84015581+aadishhere@users.noreply.github.com>
2025-05-26 09:35:48 +0530

    Optimised

--- dashboardWithSampleUI/HomeCardView.swift
+++ dashboardWithSampleUI/HomeCardView.swift
@@ -1,7 +1,30 @@
 //
-//  Untitled.swift
+//  HomeCardView.swift
 //  dashboardWithSampleUI
 //
 //  Created by Aadish Jain on 14/05/25.
 //
 
+import SwiftUI
+
+struct HomeCardView: View {
+    var title: String
+    var time: String
+    var color: Color
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+            Text(time)
+                .font(.caption)
+                .foregroundColor(.white.opacity(0.85))
+        }
+        .padding()
+        .frame(maxWidth: 200, minHeight: 100)
+        .background(color.gradient)
+        .cornerRadius(15)
+        .shadow(color: color.opacity(0.4), radius: 6, x: 0, y: 4)
+    }
+}
